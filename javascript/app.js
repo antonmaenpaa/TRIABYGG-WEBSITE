5953baa7247d246dab870e4e5396d02a44900f77
antonmaenpaa <antonmaenpaa@gmail.com>
2021-01-31 12:11:55 +0100

    homepage basic layout done

---
+++ javascript/app.js
@@ -0,0 +1,24 @@
+// window.addEventListener('load', main);
+
+// function main() {
+//     showBorderOnActiveLink();
+ 
+// }
+
+// function showBorderOnActiveLink() {
+
+
+//     var header = document.getElementById("links");
+//     var btns = header.getElementsByClassName("nav-links");
+//     for (var i = 0; i < btns.length; i++) {
+//       btns[i].addEventListener("click", function() {
+//       var current = document.getElementsByClassName("active");
+//       if (current.length > 0) { 
+//         current[0].className = current[0].className.replace(" active", "");
+//       }
+//       this.className += " active";
+//       });
+//     }
+
+//  }
+
