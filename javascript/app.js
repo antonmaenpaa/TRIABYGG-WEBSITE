18555d06ffacc7b8d55d50646c1e8d5fbf0c6136
antonmaenpaa <antonmaenpaa@gmail.com>
2021-02-02 13:31:07 +0100

    add pictures to services page, som css updates for new htma pages

--- javascript/app.js
+++ javascript/app.js
@@ -10,6 +10,8 @@ function addEventListeners() {
 
     const burgerBars = document.getElementById('burger-click');
     burgerBars.addEventListener('click', openSideBar);
+
+    window.addEventListener('scroll', changeHeaderBgOnScroll);
 }
 
 
@@ -22,6 +24,20 @@ function openSideBar() {
     } else {
       menu.style.height = 0 +'%'
     }
+}
+
+/** Fade in header background to black when scrollY is on height 600 */
+function changeHeaderBgOnScroll() {
+  const headerBlock = document.querySelector('header');
+  let top = window.scrollY;
+  if (top >= 100) {
+      headerBlock.classList.add('header-white');
+      
+  } else {
+      headerBlock.classList.remove('header-white');
+      
+  }
+
 }
 
 // function showBorderOnActiveLink() {
